d53efb9877f0cdd7cf64ed1ab108b9b21c02a185
Radoinov <radoinov08@gmail.com>
2024-02-21 09:39:04 +0200

    Da

--- ListEven/Program.cs
+++ ListEven/Program.cs
@@ -2,18 +2,27 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static List<int> listEven;
+
+        static void Main(string[] args, IEnumerable<int> listOdd)
         {
             List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Console.WriteLine("Chetni" + string.Join(" ", GetEvenElements(list)));
+            Console.WriteLine("Nechetni" + string.Join(" ", GetEvenElements(list)));
             List<int> listEven = new List<int>();
-            for (int i = 0; i < list.Count; i++)
+          
+            
+        }
+        static List<int> GetEvenElements(List<int> inputs)
+        {
+            for (int i = 0;i < inputs.Count;i++)
             {
-                if (list[i]%2 == 0)
+                if (inputs[i] % 2 == 0) 
                 {
-                    listEven.Add(list[i]);
+                    listEven.Add(inputs[i]);
                 }
             }
-            Console.WriteLine(string.Join(" ",listEven));
+            return listEven;
         }
     }
 }
